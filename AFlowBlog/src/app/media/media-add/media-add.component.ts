ffa602d8d2179bd7e6e29b575b30f24d763e37f5
homelajiang <homelajiang@163.com>
2018-10-13 18:04:11 +0800

    修改界面

--- AFlowBlog/src/app/media/media-add/media-add.component.ts
+++ AFlowBlog/src/app/media/media-add/media-add.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NzMessageService} from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-media-add',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MediaAddComponent implements OnInit {
 
-  constructor() { }
+  constructor(private msg: NzMessageService) {
+  }
 
   ngOnInit() {
   }
 
+  handleChange({file, fileList}): void {
+    const status = file.status;
+    if (status !== 'uploading') {
+      console.log(file, fileList);
+    }
+    if (status === 'done') {
+      this.msg.success(`${file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      this.msg.error(`${file.name} file upload failed.`);
+    }
+  }
 }
